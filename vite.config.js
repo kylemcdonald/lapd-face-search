8e2dddacbd44755acdbfa0b19607c87485ccc164
Kyle McDonald <kyle@kylemcdonald.net>
2025-06-21 12:26:00 -0700

    change hosting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.BUILD_TYPE === 'release' ? './' : (process.env.NODE_ENV === 'production' ? '/lapd-face-search/' : '/'),
+  base: process.env.BUILD_TYPE === 'release' ? './' : '/'
 })
